bfc85ead9d92267f401af54ad8447fc89a8c9cde
Rupesh Patil <101388825+Rupesh-1711@users.noreply.github.com>
2022-11-09 15:13:56 +0530

    first commit

---
+++ JeevanArogyaApp/src/main/java/com/jeevan/arogya/JeevanArogyaAppApplication.java
@@ -0,0 +1,13 @@
+package com.jeevan.arogya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeevanArogyaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeevanArogyaAppApplication.class, args);
+	}
+
+}
---
+++ JeevanArogyaApp/src/test/java/com/jeevan/arogya/JeevanArogyaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeevan.arogya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeevanArogyaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
